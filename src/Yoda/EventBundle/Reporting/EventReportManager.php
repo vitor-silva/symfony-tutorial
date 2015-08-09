f0fc1707c55b2909a14281280240a270a006494b
Vitor Silva <vitor.silva.inbox@gmail.com>
2015-08-09 10:02:19 +0000

    dependency injection

--- src/Yoda/EventBundle/Reporting/EventReportManager.php
+++ src/Yoda/EventBundle/Reporting/EventReportManager.php
@@ -9,14 +9,17 @@
 namespace Yoda\EventBundle\Reporting;
 
 use Doctrine\ORM\EntityManager;
+use Symfony\Component\Routing\Router;
 
 class EventReportManager
 {
     private $em;
+    private $router;
 
-    public function __construct(EntityManager $em)
+    public function __construct(EntityManager $em, Router $router)
     {
         $this->em = $em;
+        $this->router = $router;
     }
 
     public function getRecentlyUpdatedReport() {
@@ -28,7 +31,12 @@ class EventReportManager
             $data = array(
                 $event->getId(),
                 $event->getName(),
-                $event->getTime()->format('Y-m-d H:i:s')
+                $event->getTime()->format('Y-m-d H:i:s'),
+                $this->router->generate(
+                    'event_show',
+                    array('slug' => $event->getSlug()),
+                    true // generates absolute url
+                ),
             );
             $rows[] = implode(',', $data);
         }
